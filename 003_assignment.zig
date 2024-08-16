285f4a623c888586981bdd46fb7aed486bee7aee
calebsenm <calebsenm@gmail.com>
2024-08-15 23:57:56 -0500

    first commit

---
+++ 003_assignment.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn main() void {
+    var n: u8 = 50;
+    n = n + 5;
+
+    const pi: u32 = 3141519;
+    const negative_eleven: i8 = -11;
+
+    std.debug.print("{} {} {}\n", .{ n, pi, negative_eleven });
+}
---
+++ 005_arrays2.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+pub fn main() void {
+    const le = [_]u8{ 1, 2 };
+    const et = [_]u8{ 3, 4 };
+
+    const leet = le ++ et;
+    const bit_pattern = [_]u8{ 1, 0, 0, 1 } ** 3;
+
+    for (leet) |n| {
+        std.debug.print("{}", .{n});
+    }
+
+    for (bit_pattern) |n| {
+        std.debug.print("{}", .{n});
+    }
+
+    std.debug.print(" - {any} - {any} \n", .{ leet, bit_pattern });
+}
