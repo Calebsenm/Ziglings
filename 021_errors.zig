0d48139a65d1978d355815b3d1106c7e45f53d8d
calebsenm <calebsenm@gmail.com>
2024-09-12 23:35:16 -0500

    add new ziglings

---
+++ 021_errors.zig
@@ -0,0 +1,34 @@
+const MyNumberError = error{
+    TooBig,
+    TooSmall,
+    TooFour,
+};
+
+const std = @import("std");
+
+pub fn main() void {
+    const nums = [_]u8{ 2, 3, 4, 5, 6 };
+
+    for (nums) |n| {
+        std.debug.print("{}", .{n});
+
+        const number_error = numberFail(n);
+
+        if (number_error == MyNumberError.TooBig) {
+            std.debug.print(">4. ", .{});
+        }
+        if (number_error == MyNumberError.TooSmall) {
+            std.debug.print("<4. ", .{});
+        }
+        if (number_error == MyNumberError.TooFour) {
+            std.debug.print("=4. ", .{});
+        }
+    }
+}
+
+fn numberFail(n: u8) MyNumberError {
+    if (n > 4) return MyNumberError.TooBig;
+    if (n < 4) return MyNumberError.TooSmall;
+
+    return MyNumberError.TooFour;
+}
