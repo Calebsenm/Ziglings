2eb93825d76aa069393e3efbdcc3fae82de98d60
calebsenm <calebsenm@gmail.com>
2024-09-18 13:24:51 -0500

    add new ziglings

--- 005_arrays2.zig
+++ 005_arrays2.zig
@@ -7,9 +7,12 @@ pub fn main() void {
     const leet = le ++ et;
     const bit_pattern = [_]u8{ 1, 0, 0, 1 } ** 3;
 
+    std.debug.print("LEET", .{});
+
     for (leet) |n| {
         std.debug.print("{}", .{n});
     }
+    std.debug.print("Bits", .{});
 
     for (bit_pattern) |n| {
         std.debug.print("{}", .{n});
--- 011_while.zig
+++ 011_while.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn main() void {
     var n: u32 = 2;
 
-    while (n <= 1024) {
+    while (n < 1024) {
         std.debug.print("{} \n", .{n});
 
         n *= 2;
